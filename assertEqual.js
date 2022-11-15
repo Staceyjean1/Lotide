c82a9756cbab649bd6ad6f25a6241fcdb0d6b74b
Stacey Burke <stacey.burke@telus.com>
2022-11-15 13:10:08 -0800

    add assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  console.log(actual === expected);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+assertEqual(2, 4);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 2);

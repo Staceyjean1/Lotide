122e238eb7bcb315c8e433124b2670680cbe592b
Stacey Burke <stacey.burke@telus.com>
2022-11-15 13:39:45 -0800

    add head.js

---
+++ head.js
@@ -0,0 +1,8 @@
+// const assertEqual = function(actual, expected) {
+//   console.log(actual === expected);
+// };
+let array = [bananas, apples, pears]
+let first =_.head(array);
+console.log(first);
+
+

0fbf34470f84d4abff1486ae2b0147705cf00e87
Stacey Burke <stacey.burke@telus.com>
2022-11-16 20:30:21 -0800

    Add eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+
+  }
+};
+
+const eqArrays = function(firstArray, secondArray) {
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+assertEqual([1, 2, 3], [1, 2, 3]) // => true
+assertEqual([1, 2, 3], [3, 2, 1]) // => false
+
+assertEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
+assertEqual(["1", "2", "3"], ["1", "2", 3]) // => false

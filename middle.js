5f354d5eefbf7cec95cb675e5b6d4e6039d2912f
Stacey Burke <stacey.burke@telus.com>
2022-11-18 17:29:14 -0800

    Add middle.js

---
+++ middle.js
@@ -0,0 +1,41 @@
+// const assertEqual = function (actual, expected) {
+//   if (actual === expected) {
+//     console.log('✅✅✅ Assertion Passed: ${actual} === ${expected}');
+//   } else {
+//     console.log('🔴🔴🔴Assertion Failed: ${actual} !== ${expected}');
+//   }
+// };
+  
+// const eqArrays = function (firstArray, secondArray) {
+//   for (let i = 0; i < firstArray.length; i++) {
+//    if (firstArray[i] !== secondArray[i]) {
+//     return false;
+//    }
+//   return true;
+//   }
+// }
+const middle = function(array) {
+  const middleIndex = Math.floor(array.length / 2)
+  const middleTwo = array.length % 2 
+  if (array.length < 3)
+    return []
+
+  if (middleTwo === 1)
+  return [array [middleIndex]]
+    
+ if (middleTwo === 0)
+ return [array [middleIndex -1], array [middleIndex]]
+}
+//console.log(middle([1, 2, 3, 4, 5]))
+/*For arrays with one or two elements, there is no middle. Return an empty array.*/
+console.log(middle([1])) // => []
+console.log(middle([1, 2])) // => []
+
+/*For arrays with odd number of elements, an array containing a single middle element should be returned.*/
+console.log(middle([1, 2, 3])) // => [2]
+console.log(middle([1, 2, 3, 4, 5])) // => [3]
+
+//let middle = arr[Math.floor((arr.length - 1) / 2)]
+/*For arrays with an even number of elements, an array containing the two elements in the middle should be returned*/
+console.log(middle([1, 2, 3, 4])) // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]

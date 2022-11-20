011d3064f583d24c2c04cfda1af9bf48b5e64a23
Stacey Burke <stacey.burke@telus.com>
2022-11-19 21:05:16 -0800

    Add eqObjects

---
+++ eqObjects.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅✅✅ Assertion Passed: ${actual} === ${expected}');
+  } else {
+      console.log('🔴🔴🔴Assertion Failed: ${actual} !== ${expected}');
+  }
+};
+
+const eqObjects = function(object1, object2){
+  if (Object.keys(object1).length === Object.keys(object2).length){
+    for(let key of Object.keys(object1) ){
+        if(Array.isArray(object1[key]) && Array.isArray(object2[key])){
+            if (!eqArrays(object1[key],object2[key])){
+             return false;       
+            }
+        }
+        
+    }
+    return true;
+}
+return false; 
+};
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+eqObjects(ab, ba); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+eqObjects(ab, abc); // => false

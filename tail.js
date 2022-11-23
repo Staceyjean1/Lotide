f1841ebfff1f960a44e8715ed91c476b6fff337c
Stacey Burke <stacey.burke@telus.com>
2022-11-23 12:35:41 -0800

    add tail.js

---
+++ tail.js
@@ -0,0 +1,11 @@
+const assertEqual = require('./assertEqual');
+
+const tail = function (array) {
+  return array.slice(1);
+};
+
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail.js;
